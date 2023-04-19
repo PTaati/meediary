4731d5d7428d1a999e2ed70adb60ed4f703a4695
PTaati <ventotaasai@gmail.com>
2023-04-19 22:45:33 +0700

    generate spash screen change app name to MeeDiary

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:meediary/data_models/post.dart';
 import 'package:meediary/data_models/user.dart';
 import 'package:meediary/meediary_app.dart';
@@ -9,7 +10,9 @@ import 'package:provider/provider.dart';
 Future<void> main() async {
   // This is required so ObjectBox can get the application directory
   // to store the database in.
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   late ObjectBox objectbox;
   late PostService postService;
 
@@ -28,6 +31,7 @@ Future<void> main() async {
 
   postService = PostService(postBox, postBox.getAll().reversed.toList());
 
+  FlutterNativeSplash.remove();
   runApp(
     MultiProvider(
       providers: [
